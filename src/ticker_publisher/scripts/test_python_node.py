e66651825383b2e7d73e17f36b3ba5ea73b82da2
Bob Cole <colerc2@miamioh.edu>
2014-03-03 06:28:51 -0500

    ticker publishing to /ticker topic

--- src/ticker_publisher/scripts/test_python_node.py
+++ src/ticker_publisher/scripts/test_python_node.py
@@ -1,39 +1,48 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+from ticker_publisher.msg import ticker
 from collections import deque
 import btceapi
 import csv
 import time
 
-def talker():
-    pub = rospy.Publisher('chatter', String)
-    rospy.init_node('talker')
+def pub_ticker():
+    pub = rospy.Publisher('ticker', ticker)
+    rospy.init_node('ticker_node')
     
     attrs = ('high', 'low', 'avg', 'vol', 'vol_cur', 'last',
              'buy', 'sell', 'updated', 'server_time')
     
     connection = btceapi.BTCEConnection()
     
-    ticker = btceapi.getTicker("btc_usd", connection)
-    last = ticker
+    ticker_ = btceapi.getTicker("btc_usd", connection)
+    last = ticker_
 
+    msg = ticker()
     while not rospy.is_shutdown():
         try:
-            ticker = btceapi.getTicker("btc_usd", connection)
+            ticker_ = btceapi.getTicker("btc_usd", connection)
         except:
             connection = btce_api.BTCEConnection()
             pass
 
-        str = "%s" % ticker.last
-        #str = "%s" % rospy.get_time()
-        rospy.loginfo(str)
-        pub.publish(String(str))
+        msg.high = ticker_.high
+        msg.low = ticker_.low
+        msg.avg = ticker_.avg
+        msg.vol = ticker_.vol
+        msg.vol_cur = ticker_.vol_cur
+        msg.last = ticker_.last
+        msg.buy = ticker_.buy
+        msg.sell = ticker_.sell
+        msg.updated = "%s" % ticker_.updated
+        msg.server_time = "%s" % ticker_.server_time
+        pub.publish(msg)
         rospy.sleep(1.0)
 
 
 if __name__ == '__main__':
     try:
-        talker()
+        pub_ticker()
     except rospy.ROSInterruptException:
         pass
