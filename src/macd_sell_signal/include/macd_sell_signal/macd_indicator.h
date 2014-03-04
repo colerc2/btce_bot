a29f62d661b287244619ad9f1ba5338fc7411793
Bob Cole <colerc2@miamioh.edu>
2014-03-03 23:55:14 -0500

    started MACD math

--- src/macd_sell_signal/include/macd_sell_signal/macd_indicator.h
+++ src/macd_sell_signal/include/macd_sell_signal/macd_indicator.h
@@ -4,22 +4,18 @@
 
 class macd_indicator{
 private:
-  std::deque<double> short_emaf_line_;
-  std::deque<double> long_emaf_line_;
-  std::deque<double> macd_line_;
-  std::deque<double> signal_line_;
-  std::deque<double> macd_hist_;
+  double short_emaf_line_;
+  double long_emaf_line_;
+  double macd_line_;
+  double signal_line_;
+  double macd_hist_;
 
-  int short_, long_, sig_, period_, spread_window_, size_backlog_;
+  int short_, long_, sig_, period_;
   double short_alpha_, long_alpha_, signal_alpha_;
+
 public:
-  macd_indicator(){}
+  macd_indicator();
   macd_indicator(int short_len, int long_len, int sig_len, int period_len, int spread_window_len);
   int update(double new_data);
   
-  std::deque<double> get_short_emaf_line();
-  std::deque<double> get_long_emaf_line();
-  std::deque<double> get_macd_line();
-  std::deque<double> get_signal_line();
-  std::deque<double> get_macd_hist_();
 };
--- src/macd_sell_signal/src/macd_indicator.cpp
+++ src/macd_sell_signal/src/macd_indicator.cpp
@@ -9,12 +9,17 @@ macd_indicator::macd_indicator(int short_len, int long_len, int sig_len, int per
   this->long_ = long_len;
   this->sig_ = sig_len;
   this->period_ = period_len;
-  this->spread_window_ = spread_window_len;
   
   //alpha for EMA calculated with N points, the last N datum points in an EMA represent about 86% of the total weight in the calculation (this can be changed, but i don't really see any reason to)
   this->short_alpha_ = 2.0/((this->short_ + 1) * this->period_);  
   this->long_alpha_ = 2.0/((this->long_ + 1) * this->period_);
   this->sig_alpha_ = 2.0/((this->sig_ + 1) * this->period_);
+  
+  this->short_emaf_line_ = 0;
+  this->long_emaf_line_ = 0;
+  this->macd_line_ = 0;
+  this->signal_line_ = 0;
+  this->macd_hist_ = 0;
 
 }
 /*short_emaf = exponential_moving_average(data,(2/((short+1)*delta_t)));
@@ -23,23 +28,20 @@ macd_indicator::macd_indicator(int short_len, int long_len, int sig_len, int per
     signal_line = exponential_moving_average(macd_line,(2/((signal+1)*delta_t)));
     macd = macd_line - signal_line;*/
 
-int macd_indicator::update(double new_data){
-  
-  return 0;
-}
+int macd_indicator::update(double new_data, double &short_emaf, double &long_emaf, double &macd, double &signal, double &macd_hist){
 
-std::deque<double> macd_indicator::get_short_emaf_line(){
-  return this->short_emaf_line_;
-}
-std::deque<double> macd_indicator::get_long_emaf_line(){
-  return this->long_emaf_line_;
-}
-std::deque<double> macd_indicator::get_macd_line(){
-  return this->macd_line_;
-}
-std::deque<double> macd_indicator::get_signal_line(){
-  return this->signal_line_line_;
-}
-std::deque<double> macd_indicator::get_macd_hist_(){
-  return this->short_emaf_line_;
+  short_emaf = (this->short_alpha*new_data) + ((1-this->short_alpha_)*this->short_emaf_line_);
+  long_emaf = (this->long_alpha*new_data) + ((1-this->long_alpha_)*this->long_emaf_line_);
+  macd = short_emaf-long_emaf;
+  signal = (this->sig_alpha_ * macd) + ((1-this->sig_alpha_) * this->signal_line_);
+  macd_hist = macd - signal;
+
+  //update objects local values with new information
+  this->short_emaf_line_ = short_emaf;
+  this->long_emaf_line_ = long_emaf;
+  this->macd_line_ = macd;
+  this->signal_line_ = signal;
+  this->macd_hist_ = macd_hist;
+
+  return 0;
 }
