1a7655189a60edf43d65b383cb566fc332b01a95
Bob Cole <colerc2@miamioh.edu>
2014-03-05 02:41:38 -0500

    added MACD service and changed plotting code accordingly

--- src/macd_sell_signal/src/macd_sell_signal_node.cpp
+++ src/macd_sell_signal/src/macd_sell_signal_node.cpp
@@ -1,18 +1,23 @@
 #include <ros/ros.h>
 #include <ros/time.h>
 #include <string>
-#include "ticker_publisher/ticker.h"
-#include <macd_sell_signal/macd_indicator.h>
-#include "macd_sell_signal/macd.h"
+#include <vector>
+#include <macd_sell_signal/macd_indicator.h>//macd_indicator object
+#include "ticker_publisher/ticker.h"//ticker msg
+#include "macd_sell_signal/macd.h"//macd msg
+#include "macd_sell_signal/macd_array.h"//macd array service
 
-//TODO: make these a circular buffer/queue
+//passed by reference to the macd_indicator object
 double short_emaf_line_, long_emaf_line_, macd_line_, signal_line_, macd_hist_;
 
+//keeping vector of old points around for service
+std::vector<macd_sell_signal::macd> old_points_;
+
 //MACD object
-macd_indicator* macd_;
+macd_indicator macd_;
 
 //parameters read in at runtime
-int short_, long_, sig_, period_, spread_window_;
+int short_, long_, sig_, period_, num_old_periods_, spread_window_;
 double spread_value_;
 std::string trade_pair_;
 
@@ -25,19 +30,35 @@ void ticker_callback(const ticker_publisher::ticker::ConstPtr &msg){
   //	   msg->high, msg->low, msg->avg, msg->vol, msg->vol_cur, msg->last, msg->buy, msg->sell);
  
   //grab data from msg and pass through MACD
-  //ticker_publisher::ticker ticker_msg = *msg;
-  macd_->update(msg->last, short_emaf_line_, long_emaf_line_, macd_line_, signal_line_, macd_hist_);
+  macd_.update(msg->last, short_emaf_line_, long_emaf_line_, macd_line_, signal_line_, macd_hist_);
   //ROS_INFO("(%f,%f,%f,%f,%f)",short_emaf_line_, long_emaf_line_, macd_line_, signal_line_, macd_hist_);
   
-  //publish MACD info
+  //publish MACD info for current time
   macd_sell_signal::macd macd_msg;
+  //header
   macd_msg.header.stamp = ros::Time::now();
+  //save ticker values
+  macd_msg.tick = *msg;
+  //macd stuff
   macd_msg.short_emaf = short_emaf_line_;
   macd_msg.long_emaf = long_emaf_line_;
   macd_msg.macd = macd_line_;
   macd_msg.signal = signal_line_;
   macd_msg.macd_hist = macd_hist_;
   macd_pub_.publish(macd_msg);
+
+  //pop front, push back
+  if(old_points_.size() > num_old_periods_*period_){
+    old_points_.erase(old_points_.begin());
+  }
+  old_points_.push_back(macd_msg);
+}
+
+bool request_array(macd_sell_signal::macd_array::Request &req,
+		   macd_sell_signal::macd_array::Response &res){
+  res.macd_array = old_points_;
+
+  return true;
 }
 
 int main(int argc, char** argv){
@@ -52,6 +73,7 @@ int main(int argc, char** argv){
   nh.param("long", long_, 0);
   nh.param("sig", sig_, 0);
   nh.param("period", period_, 0);
+  nh.param("num_old_periods", num_old_periods_, 0);
   nh.param("spread_window", spread_window_, 0);
   nh.param("spread_value", spread_value_, 0.0);
 
@@ -63,15 +85,24 @@ int main(int argc, char** argv){
   //Publishers
   std::string macd_topic = "macd_" + std::to_string(short_) + 
     "_" + std::to_string(long_) + "_" + std::to_string(sig_) +
-    "_x" + std::to_string(period_);
+    "_x" + std::to_string(period_) + "/macd";
   macd_pub_ = n.advertise<macd_sell_signal::macd>(macd_topic, 10);
   
+  //Services
+  std::string macd_array_service = "macd_" + std::to_string(short_) + 
+    "_" + std::to_string(long_) + "_" + std::to_string(sig_) +
+    "_x" + std::to_string(period_) + "/macd_array";
+  ros::ServiceServer service = n.advertiseService(macd_array_service, request_array);
+  ROS_INFO("Ready to provide macd_array service.");
+
   //intialize globals
-  macd_ = new macd_indicator(short_, long_, sig_, period_);
+  macd_ = macd_indicator(short_, long_, sig_, period_);
  
  
   //
   ros::Rate rate(100);
+  //only use one thread for callbacks, that was the service never responds
+  //while the array is being manipulated
   ros::AsyncSpinner spinner(1);
   spinner.start();
   while(ros::ok()){
--- src/plot_macd/scripts/plot_macd_node.py
+++ src/plot_macd/scripts/plot_macd_node.py
@@ -3,6 +3,7 @@ import rospy
 import std_msgs.msg
 from ticker_publisher.msg import ticker
 from macd_sell_signal.msg import macd
+from macd_sell_signal.srv import macd_array
 import numpy as np
 import matplotlib
 matplotlib.use('GTKAgg')
@@ -13,17 +14,79 @@ from collections import deque
 import time
 import datetime
 
-#macd buffers
-buffer_short_emaf = deque([])
-buffer_long_emaf = deque([])
-buffer_macd = deque([])
-buffer_signal = deque([])
-buffer_macd_hist = deque([])
-#ticker buffers
-buffer_last = deque([])
-buffer_server_time = deque([])
 
 fig=plt.figure()
+
+def get_data_and_plot(service):
+    rospy.wait_for_service(service)
+    try:
+        #call service
+        grab_array = rospy.ServiceProxy(service, macd_array)
+        macd_array_ = grab_array()
+        
+        #populate new arrays each time, this is so god damn inefficient, oh well
+        #macd buffers
+        buffer_short_emaf = deque([])
+        buffer_long_emaf = deque([])
+        buffer_macd = deque([])
+        buffer_signal = deque([])
+        buffer_macd_hist = deque([])
+        #ticker buffers
+        buffer_last = deque([])
+        buffer_server_time = deque([])
+        
+        for i in range(len(macd_array_.macd_array)):
+            macd_msg = macd_array_.macd_array[i]
+            buffer_short_emaf.append(macd_msg.short_emaf)
+            buffer_long_emaf.append(macd_msg.long_emaf)
+            buffer_macd.append(macd_msg.macd)
+            buffer_signal.append(macd_msg.signal)
+            buffer_macd_hist.append(macd_msg.macd_hist)
+            tmp_date = datetime.datetime.strptime(macd_msg.tick.server_time, "%Y-%m-%d %H:%M:%S")
+            buffer_server_time.append(tmp_date)
+            buffer_last.append(macd_msg.tick.last)
+
+        #plot her
+        plt.clf()
+        if(len(macd_array_.macd_array) > 0):
+            plt.subplot(211)
+            p1 = plt.plot(buffer_server_time,buffer_last,'g',linewidth=4.0)
+            p2 = plt.plot(buffer_server_time,buffer_short_emaf,'b',linewidth=3.0)
+            p3 = plt.plot(buffer_server_time,buffer_long_emaf,'r',linewidth=2.0)
+            plt.xlabel('Time')
+            plt.ylabel('Price ($)')
+            plt.grid(True)
+            plt.legend(['Last', '12-period EMAF', '26-period EMAF'], loc=2)
+            min_y = min([min(buffer_short_emaf), min(buffer_long_emaf), min(buffer_last)])
+            max_y = max([max(buffer_long_emaf), max(buffer_short_emaf), max(buffer_last)])
+            #next 3 lines are for pretty
+            range_y = 0.1*(max_y-min_y)
+            min_y -= range_y
+            max_y += range_y
+            plt.ylim([min_y, max_y])
+            plt.gcf().autofmt_xdate()
+            plt.gca().get_yaxis().get_major_formatter().set_useOffset(False)
+            plt.subplot(212)
+            plt.plot(buffer_server_time,buffer_macd,'b',linewidth=2.0)
+            plt.plot(buffer_server_time,buffer_signal,'r',linewidth=2.0)
+            plt.plot(buffer_server_time,buffer_macd_hist,'k',linewidth=4.0)
+            plt.xlabel('Time');
+            plt.ylabel('wat');
+            plt.grid(True)
+            plt.legend(['MACD', 'Signal', 'Histogram'], loc=2)
+            min_y = max([abs(min(buffer_macd)), abs(min(buffer_signal)), abs(min(buffer_macd_hist))])
+            max_y = max([max(buffer_macd), max(buffer_signal), max(buffer_macd_hist), min_y])*1.1
+            plt.ylim([-max_y, max_y])
+            plt.gcf().autofmt_xdate()
+            fig = plt.gcf()
+            fig.set_size_inches(15,10)
+            plt.draw()
+            time.sleep(0.1)
+
+            
+            #rospy.loginfo(macd_msg)
+    except rospy.ServiceException, e:
+            print "Service call failed: %s" % e
 
 def macd_callback(data):
     global buffer_short_emaf
@@ -102,13 +165,17 @@ def plot():
     plt.ion()
     plt.show()
     #get params
-    ticker_topic = rospy.get_param('~ticker_topic', 'error')
-    macd_topic = rospy.get_param('~macd_topic', 'error')
+    macd_array_service = rospy.get_param('~macd_array_service', 'error')
+    #ticker_topic = rospy.get_param('~ticker_topic', 'error')
+    #macd_topic = rospy.get_param('~macd_topic', 'error')
     #create subscribers
-    rospy.Subscriber(ticker_topic, ticker, ticker_callback)
-    rospy.Subscriber(macd_topic, macd, macd_callback)
+    #rospy.Subscriber(ticker_topic, ticker, ticker_callback)
+    #rospy.Subscriber(macd_topic, macd, macd_callback)
     #wait for new messages
-    rospy.spin()
+    #rospy.spin()
+    while 1:
+        get_data_and_plot(macd_array_service)
+        time.sleep(5.0)
     
 
 
