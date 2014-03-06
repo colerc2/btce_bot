b9929604d34ee2cdf2e6620dd44d8de717cf051e
Bob Cole <colerc2@miamioh.edu>
2014-03-06 02:43:59 -0500

    added file output for save_load_ticker

--- src/save_load_ticker/src/save_load_ticker_node.cpp
+++ src/save_load_ticker/src/save_load_ticker_node.cpp
@@ -11,22 +11,35 @@
 
 std::vector<std::string> tickers_;
 std::vector<ros::Subscriber> subs_;
-std::map<std::string, std::ofstream*> file_handles_;//have to keep pointers to ofstream unfortunately
+std::map<std::string, std::fstream*> file_handles_;//have to keep pointers to ofstream unfortunately
+std::string base_file_;
 
 void ticker_callback(const ticker_publisher::ticker::ConstPtr &message){
-  //find what pair this is from
   ticker_publisher::ticker msg = *message;
-  std::string filename = msg.trade_pair + "_" + msg.server_time.substr(0,10) + ".tkr";
+  
+  //construct filename
+  std::string filename = base_file_ + msg.trade_pair + "/" + 
+    msg.trade_pair + "_" + msg.server_time.substr(0,10) + ".tkr";
   std::replace(filename.begin(), filename.end(), '-', '_');//replace hyphens with underscores
-  std::ofstream *file_handle;
-  std::map<std::string, std::ofstream*>::const_iterator it = file_handles_.find(filename);
+  std::fstream *file_handle;
+  std::map<std::string, std::fstream*>::const_iterator it = file_handles_.find(filename);
   if(it == file_handles_.end()){//need to create new file handle
-    file_handle->open(filename.c_str());
+    file_handle = new std::fstream(filename.c_str(),std::ios::app | std::ios::out);
     file_handles_[filename] = file_handle;
     ROS_INFO("New file handle created for pair %s: %s", msg.trade_pair.c_str(), filename.c_str());
   }else{//use old file handle
     file_handle = (file_handles_[filename]);
   }
+
+  if(msg.trade_pair == "btc_usd"){
+    //ROS_INFO("Callback for bitcoin");
+  }
+
+  //save data to file
+  (*file_handle) << msg.high << "," << msg.low << "," << msg.avg << "," << msg.vol << "," <<
+    msg.vol_cur << "," << msg.last << "," << msg.buy << "," << msg.sell << "," << msg.updated << "," <<
+    msg.server_time << "\n";
+  file_handle->flush();//write to file each time (slower but better for when data is read later)
   
   
 }
@@ -50,7 +63,9 @@ void check_for_new_tickers(ros::NodeHandle &n){
     if(topics[i].substr(0,8) == "/ticker_"){
       //if not subscribed to this one, subscribe to it
       if(std::find(tickers_.begin(), tickers_.end(), topics[i]) == tickers_.end()){
+	ROS_INFO("subscribing to %s", topics[i].c_str());
 	subs_.push_back(n.subscribe(topics[i], 1, ticker_callback));
+	tickers_.push_back(topics[i]);
       }
     }
   }
@@ -61,6 +76,9 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "save_load_ticker_node");
   ros::NodeHandle n;//global
   ros::NodeHandle nh("~"); //local
+
+  //TODO:make this a param
+  base_file_ = "/home/bob/Documents/btce_bot/ticker_data/";
 
   ROS_INFO("Checking for new tickers");
   //check to see what tickers are running, populate vector
