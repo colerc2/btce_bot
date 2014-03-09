f3e225d128d9c5b2dfb64bdf99e4696b0922b066
Bob Cole <cole.robert.c@gmail.com>
2014-03-09 13:16:39 -0400

    example btce interface node running, doesn't do much yet

--- src/trade_interface/scripts/test_interface.py
+++ src/trade_interface/scripts/test_interface.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python
 import rospy
+import roslib
 import std_msgs.msg
 import btceapi
 import csv
@@ -8,8 +10,33 @@ import sys
 class InterfaceNode():
     def __init__(self):
         #get params
-        
-    
+        #TODO: make this a param
+        key_file = "/home/bob/Documents/btce_bot/trade_api.key"
+        handler = btceapi.KeyHandler(key_file, resaveOnDeletion=True)
+        for key in handler.getKeys():
+            print "Printing info for key %s" % key
+            
+            # NOTE: In future versions, the handler argument will be required.
+            conn = btceapi.BTCEConnection()
+            t = btceapi.TradeAPI(key, handler=handler)
+            
+            try:
+                r = t.getInfo(connection = conn)
+                
+                for currency in btceapi.all_currencies:
+                    balance = getattr(r, "balance_" + currency)
+                    print "\t%s balance: %s" % (currency.upper(), balance)
+                    print "\tInformation rights: %r" % r.info_rights
+                    print "\tTrading rights: %r" % r.trade_rights
+                    print "\tWithrawal rights: %r" % r.withdraw_rights
+                    print "\tServer time: %r" % r.server_time
+                    print "\tItems in transaction history: %r" % r.transaction_count
+                    print "\tNumber of open orders: %r" % r.open_orders
+                        
+            except Exception as e:
+                print "  An error occurred: %s" % e
+                raise e
+            
 
 if __name__ == '__main__':
     #intialize node and name her
