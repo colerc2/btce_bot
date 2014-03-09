7766067d05d8d136fa709fadbbaf1b01d2de22c6
colerc2 <cole.robert.c@gmail.com>
2014-03-09 12:56:35 -0400

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -2,6 +2,6 @@
 git pull
 git add -A
 git config --global user.name "Bob Cole"
-git config --global user.email colerc2@miamioh.edu
+git config --global user.email cole.robert.c@gmail.com
 git commit -a --author="Bob Cole <cole.robert.c@gmail.com>"
 git push
