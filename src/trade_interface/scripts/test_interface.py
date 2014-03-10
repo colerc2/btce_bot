d2a6765530fb9ac287290ecb944b9c2e1d8a5cc2
Bob Cole <cole.robert.c@gmail.com>
2014-03-09 22:04:57 -0400

    publishers for get_info class in test_interface.py working

--- src/trade_interface/scripts/test_interface.py
+++ src/trade_interface/scripts/test_interface.py
@@ -6,6 +6,8 @@ import btceapi
 import csv
 import time
 import sys
+from trade_interface.msg import get_info
+from collections import deque
 
 class InterfaceNode():
     def __init__(self):
@@ -14,9 +16,12 @@ class InterfaceNode():
         key_file = "/home/bob/Documents/btce_bot/trade_api.key"
         handler = btceapi.KeyHandler(key_file, resaveOnDeletion=True)
         conn = btceapi.BTCEConnection()
+        topic_names_init = False
+        topics = deque([])
+        msg = get_info()
 
         while not rospy.is_shutdown():
-            rospy.sleep(2.0)
+            rospy.sleep(5.0)
             for key in handler.getKeys():
                 print "Printing info for key %s" % key
                 
@@ -26,22 +31,36 @@ class InterfaceNode():
                 try:
                     r = t.getInfo(connection = conn)
                     
+                    counter = 0
                     for currency in btceapi.all_currencies:
+                        if (topic_names_init == False):
+                            topic_name = 'get_info/' + currency.upper()
+                            topics.append(rospy.Publisher(topic_name, get_info))
+                                          
                         balance = getattr(r, "balance_" + currency)
-                        print "\t%s balance: %s" % (currency.upper(), balance)
-                        print "\tInformation rights: %r" % r.info_rights
-                        print "\tTrading rights: %r" % r.trade_rights
-                        print "\tWithrawal rights: %r" % r.withdraw_rights
-                        print "\tServer time: %r" % r.server_time
-                        print "\tItems in transaction history: %r" % r.transaction_count
-                        print "\tNumber of open orders: %r" % r.open_orders
-                        
+                        msg.coin = currency.upper()
+                        msg.balance = balance
+                        test_string = "%r" % r.server_time
+                        msg.server_time = r.server_time.strftime("%Y-%m-%d %H:%M:%S")
+                        topics[counter].publish(msg)
+                        counter = counter + 1
+                                          #print "\t%s balance: %s" % (currency.upper(), balance)
+                        #print "\tInformation rights: %r" % r.info_rights
+                        #print "\tTrading rights: %r" % r.trade_rights
+                        #print "\tWithrawal rights: %r" % r.withdraw_rights
+                        #print "\tServer time: %r" % r.server_time
+                        #print "\tItems in transaction history: %r" % r.transaction_count
+                        #print "\tNumber of open orders: %r" % r.open_orders
+                                          
+                    topic_names_init = True
+                                             
                 except Exception as e:
                     print "  An error occurred: %s" % e
-                    rospy.sleep(2.0)
+                    rospy.sleep(5.0)
 
-                    conn = btceapt.BTCEConnection()
+                    conn = btceapi.BTCEConnection()
                     pass
+                                          
 #raise e
                 
 
